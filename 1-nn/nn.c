dfa35a53d514ef48c9c92e90b62dd92b512e61d9
zxf8876983 <zxf8876983@163.com>
2017-11-06 21:12:14 +0800

    int

--- 1-nn/nn.c
+++ 1-nn/nn.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<obliv.h>
-
+#include <time.h>
 #include"nn.h"
 #include"../../obliv-c/test/oblivc/common/util.h"
 
@@ -8,20 +8,85 @@ int main(int argc,char *argv[])
 {
   ProtocolDesc pd;
   protocolIO io;
+  FILE *fp;
+  float w[w_row][w_col];
+  float b[b_row][b_col];
+  float x[x_row][x_col];
+  int i,j,k;
+  int t=10000;
+/*
   if(argc<4)
   { if(argc<2) fprintf(stderr,"Port number missing\n");
     else if(argc<3) fprintf(stderr,"Server location missing\n");
     else fprintf(stderr,"string missing\n");
     fprintf(stderr,"Usage: %s <port> <--|remote_host> <int>\n",argv[0]);
     return 1;
-  }
-  sscanf(argv[3],"%f",&io.value);
-  const char* remote_host = (strcmp(argv[2],"--")?argv[2]:NULL);
-  ocTestUtilTcpOrDie(&pd,remote_host,argv[1]);
-  int currentParty = (remote_host?2:1);
+  }*/
+
+
+const char* remote_host = (strcmp(argv[2],"--")?argv[2]:NULL);
+ocTestUtilTcpOrDie(&pd,remote_host,argv[1]);
+int currentParty = (remote_host?2:1);
+
+
+if(currentParty==1){
+ if((fp=fopen("parameter/W.txt","r"))==NULL)
+ { 
+  printf(" can't open");
+  exit(0);
+ }else{
+   for(i=0;i<w_row;i++){
+    for(j=0;j<w_col;j++){
+     fscanf(fp,"%f",&w[i][j]);
+     io.w[i][j]=(int)(w[i][j]*t);
+    }
+   }
+ fclose(fp);
+}
+
+ if((fp=fopen("parameter/b.txt","r"))==NULL)
+ { 
+  printf(" can't open");
+  exit(0);
+ }else{
+   for(i=0;i<b_row;i++){
+    for(j=0;j<b_col;j++){
+     fscanf(fp,"%f",&b[i][j]);
+     io.b[i][j]=(int)(b[i][j]*t);
+     //printf("%d\t",b[i][j]);
+    }
+   }
+ fclose(fp);
+}
+}
+ 
+if(currentParty==2){
+ if((fp=fopen("parameter/x.txt","r"))==NULL)
+ { 
+  printf(" can't open");
+  exit(0);
+ }else{
+   for(i=0;i<x_row;i++){
+    for(j=0;j<x_col;j++){
+     fscanf(fp,"%f",&x[i][j]);
+     io.x[i][j]=(int)(x[i][j]*t);
+    }
+   }
+ fclose(fp);
+}
+}
+
+  clock_t start, end;
+  start = clock();
+  //sscanf(argv[3],"%f",&io.value);
   setCurrentParty(&pd,currentParty);
   execYaoProtocol(&pd,nn,&io);
   cleanupProtocol(&pd);
-  fprintf(stderr,"Result: %f\n",io.res);
+  for(i=0;i<10;i++){
+    fprintf(stderr,"Result: %d\n",io.res[i]);
+  }
+  end = clock();
+  double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  fprintf(stderr,"Elapsed Time: %f\n",cpu_time_used);
   return 0;
 }
--- 1-nn/nn.h
+++ 1-nn/nn.h
@@ -1,6 +1,15 @@
+#define w_row 784
+#define w_col 10
+#define b_row 1
+#define b_col 10
+#define x_row 10
+#define x_col 784
+#define y_row 10
+#define y_col 10
+
 typedef struct protocolIO
-{ float value;
-  float res;
+{ int w[w_row][w_col],b[b_row][b_col],x[x_row][x_col];
+  int res[10];
 } protocolIO;
 
 void nn(void* args);
